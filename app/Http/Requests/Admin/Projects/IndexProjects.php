171d728ee5631841ce871f3e7d61859e84a76bae
Pedro Acosta <pacosta@muvh.gov.py>
2022-08-10 21:12:05 -0400

    ajustes indexprojects

--- app/Http/Requests/Admin/Projects/IndexProjects.php
+++ app/Http/Requests/Admin/Projects/IndexProjects.php
@@ -25,7 +25,7 @@ class IndexProjects extends FormRequest
     public function rules(): array
     {
         return [
-            'orderBy' => 'in:id',
+            'orderBy' => 'in:SEOBId|nullable',
             'orderDirection' => 'in:asc,desc|nullable',
             'search' => 'string|nullable',
             'page' => 'integer|nullable',
